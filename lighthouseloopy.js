1dc984bad4ed19cee8d803bff2efcbbf9802e366
tomsecret <bladelun@gmail.com>
2017-06-17 19:37:42 +0000

    Add loopylighthouse

---
+++ lighthouseloopy.js
@@ -0,0 +1,14 @@
+for (var n = 100; n <= 200; n++){
+  if (n % 3 == 0 && n % 4 == 0){
+  console.log('LoopyLighthouse');
+  }
+  if (n % 3 == 0){
+  console.log('Loopy');
+  }
+  if (n % 4 == 0){
+  console.log('lighthouse');
+  }
+  else {
+  console.log(n);
+  }
+};
